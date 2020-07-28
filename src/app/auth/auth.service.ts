bd0d2afd31d857a74b776bb62988c5a94fe8c6f2
Sam <samshum90@gmail.com>
2020-07-28 20:14:33 +0100

    added login page

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
-interface AuthResponseData {
+export interface AuthResponseData {
     kind: string;
     idToken: string;
     email: string;
     refreshToken: string;
     expiresIn: string;
     localId: string;
+    registered?: boolean;
 }
 
 @Injectable({providedIn: 'root'})
@@ -24,16 +25,28 @@ export class AuthService {
             returnSecureToken: true
         }
         )
-        .pipe(catchError(errorRes => {
-            let errorMessage = 'An unknown error Occurred!';
-            if( !errorRes.error || !errorRes.error.error ){
-                return throwError(errorMessage)
-            }
-            switch(errorRes.error.error.message) {
-                case 'EMAIL_EXISTS':
-                    errorMessage = 'This email already exists'
-            }
-            return throwError(errorMessage);
-        }));
+        .pipe(catchError(this.handleError));
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDRXDoE1EwfNYCKIcqejYud_G0EJI1R_Ak', 
+        {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        })
+        .pipe(catchError(this.handleError));
+    }
+
+    private handleError(errorRes: HttpErrorResponse){
+        let errorMessage = 'An unknown error Occurred!';
+        if( !errorRes.error || !errorRes.error.error ){
+            return throwError(errorMessage)
+        }
+        switch(errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+                errorMessage = 'This email already exists'
+        }
+        return throwError(errorMessage);
     }
 }
