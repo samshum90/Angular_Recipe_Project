4a74caf16fb10c44cc7f51a4c4bb6c663a6b2d56
Sam <samshum90@gmail.com>
2020-08-07 21:24:45 +0100

    auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-
-import { AuthComponent } from './auth/auth.component';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 
@@ -20,16 +17,14 @@ import { CoreModule } from './core.module';
     AppComponent,
     TitleComponent,
     HeaderComponent, 
-    AuthComponent,
   ],
   imports: [
     BrowserModule, 
-    FormsModule, 
-    ReactiveFormsModule, 
     HttpClientModule, 
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
+    AuthModule,
     SharedModule,
     CoreModule
   ],
